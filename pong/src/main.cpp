6d5a3b007b92d5d6ee2b1ac025806abdf578488c
Christopher Keh <ckeh6336@gmail.com>
2020-09-24 19:21:50 -0700

    removed ball from falling block, fixed spacing in main.cpp

--- pong/src/main.cpp
+++ pong/src/main.cpp
@@ -4,41 +4,39 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800,600), "MyGame!");
+  sf::RenderWindow window(sf::VideoMode(800,600), "MyGame!");
 	window.setFramerateLimit(60);
 	window.requestFocus();
-    Ball ball;
-	while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-				ball.bmotion.up = 1;
-				ball.move(0,-ball.getVel());	
-			}
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-				ball.move(0,ball.getVel());	
-			}
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-				ball.move(-ball.getVel(),0);	
-			}
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-				ball.move(ball.getVel(),0);	
-			}
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)){
-				ball.decreVel();	
-			}
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)){
-				ball.incrVel();	
-			}
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-		ball.update(&window, );
-        window.display();
+  Ball ball;
+	while (window.isOpen()){
+    sf::Event event;
+    while (window.pollEvent(event)){
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+        ball.bmotion.up = 1;
+        ball.move(0,-ball.getVel());	
+      }
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+        ball.move(0,ball.getVel());	
+      }
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+        ball.move(-ball.getVel(),0);	
+      }
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+        ball.move(ball.getVel(),0);	
+      }
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)){
+        ball.decreVel();	
+      }
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)){
+        ball.incrVel();	
+      }
+      if (event.type == sf::Event::Closed)
+        window.close();
     }
+    window.clear();
+		ball.update(&window, );
+    window.display();
+  }
 
-    return 0;
+return 0;
 }
